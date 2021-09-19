4d899e8c3f76e47e61311ab4dae0fd085edeb920
Sabitov Marat <sabitov.m.a@yandex.ru>
2021-09-19 18:38:37 +0500

    aboute page done'

--- src/components/About/AboutContainer.js
+++ src/components/About/AboutContainer.js
@@ -26,7 +26,7 @@ export const Wallpaper = styled.div`
 
 export const AboutItem = styled.h2`
     font-weight: 500;
-    grid-column: 2 / span 6;
+    grid-column: ${props => props.column};
     grid-row: ${props => props.row};
     border: solid black 2px;
     border-radius: 30px;
@@ -36,18 +36,25 @@ export const AboutItem = styled.h2`
     display: flex;
     justify-content: flex-start;
     align-items: center;
-    padding-left: 1rem;
+    padding: 0.5rem 1rem;
     position: relative;
+    cursor: default;
+    background-color: ${props => props.active};
 
     &:hover, &:active, &:focus {
         box-shadow: 5px 5px 10px grey;
     } 
 
-    @media screen and (max-width: 1280px){
+    @media screen and (max-width: 1305px){
         font-size: 0.8rem;
     }
+    @media screen and (max-width: 950px){
+        font-size: 0.7rem;
+        
+    }
     @media screen and (max-width: 768px){
-        font-size: 0.8rem;
+        font-size: 0.5rem;
+        padding: 0 0.5rem;
         grid-column: 2 / span 8;
     }
             
@@ -58,4 +65,65 @@ export const ItemIcon = styled.div`
     height: 24px;
     margin-right: 20px;
     z-index: 55;
+
+    @media screen and (max-width: 768px){
+        margin-right: 10px;
+    }
+`;
+
+export const DepImgCont = styled.div`
+    grid-column: 7 / span 3;
+    grid-row: 5 / span 5;
+    z-index: 10;
+    width: 100%;
+    height: 100%;
+    background-image: url(${({url}) => url});
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    overflow: hidden;
+
+    @media screen and (max-width: 768px){
+        grid-column: 6 / span 4;
+        grid-row: 2 / span 3;
+        margin: 10px;
+    }
+
+`;
+
+export const Resume = styled.div`
+    display: none;
+
+    @media screen and (max-width: 768px){
+        display: block;
+        grid-column: 2 / span 4;
+        grid-row: 2 / span 3;
+        margin: 10px;
+        z-index: 10;
+        width: 100%;
+        height: 100%;
+        background-image: url(${({url}) => url});
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+        overflow: hidden;
+    }
+
+`;
+
+export const AboutHeading = styled.h1`
+    font-weight: 700;
+    grid-column: 2 / span 8;
+    grid-row: 3 / span 2;
+    color: black;
+    font-size: 1.5rem;
+    z-index: 50;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0.5rem 1rem;
+
+    @media screen and (max-width: 768px){
+        display: none;
+    }
 `;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,52 +1,46 @@
 import React, {useState} from 'react';
-import {AboutContainer, AboutItem, ItemIcon, Wallpaper } from '../components/About/AboutContainer';
-import {FaBirthdayCake } from 'react-icons/fa';
+import {AboutContainer, AboutItem, DepImgCont, ItemIcon, Wallpaper, Resume, AboutHeading } from '../components/About/AboutContainer';
+import {FaBirthdayCake, FaBookOpen, FaBook, FaFlag, FaUniversity, FaUserTie, FaRocket } from 'react-icons/fa';
+import DepImage0 from '../images/svg/DepImage-0.svg';
+import DepImage1 from '../images/svg/DepImage-1.svg';
+import DepImage2 from '../images/svg/DepImage-2.svg';
+import DepImage3 from '../images/svg/DepImage-3.svg';
+import DepImage4 from '../images/svg/DepImage-4.svg';
+import DepImage5 from '../images/svg/DepImage-5.svg';
+import DepImage6 from '../images/svg/DepImage-6.svg';
+import DepImage7 from '../images/svg/DepImage-7.svg';
+import ResImg from '../images/svg/Resume.svg';
+
 const About = () => {
+    const [numItem, setNumItem] = useState(0);
+    const itemInfo = [
+        {column:"2 / span 5", row:"5 / span 1", text:"Дата рождения: 3 сентября 1997 г.", jsx: <FaBirthdayCake />}, 
+        {column:"2 / span 5", row:"6 / span 1", text:"Семейное положение: Холост", jsx: <FaUserTie />}, 
+        {column:"2 / span 5", row:"7 / span 1", text:"Гражданство: Российская Федерация", jsx: <FaFlag />}, 
+        {column:"2 / span 5", row:"8 / span 1", text:"Служба в армии: 2020-2021 гг. (научная рота)", jsx: <FaRocket />},
+        {column:"2 / span 5", row:"9 / span 1", text:"Образование: Высшее", jsx: <FaUniversity />}, 
+        {column:"2 / span 8", row:"10 / span 1", text:"Автоматизация технологических процессов и производств, 2015-2019 гг.", jsx: <FaBook />}, 
+        {column:"2 / span 8", row:"11 / span 1", text:"Информатика и вычислительная техника, 2019-2022 гг.", jsx: <FaBookOpen />}
+    ];
+    
+    const itemList = itemInfo.map((item, ind) => (
+        <AboutItem onClick={numItem => (numItem != (ind+1)) ? setNumItem(ind+1) : setNumItem(0)} active={numItem == (ind+1) ? 'white' : 'none'} column={item.column} row={item.row}>
+            <ItemIcon>
+                {item.jsx}
+            </ItemIcon>
+            {item.text}
+        </AboutItem>
+    ));
+
+    const imgArray = [DepImage0, DepImage1, DepImage2, DepImage3, DepImage4, DepImage5, DepImage6, DepImage7];
+
     return (
         <AboutContainer>
-            <Wallpaper />
-            <AboutItem column="2 / span 5" row="4 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Дата рождения: 3 сентября 1997 г.
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="5 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Семейное положение: Холост
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="6 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Гражданство: Российская Федерация
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="7 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Служба в армии: 2020-2021 гг. (научная рота)
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="8 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Образование: Высшее
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="9 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Автоматизация технологических процессов и производств, 2015-2019 гг.
-            </AboutItem>
-            <AboutItem column="2 / span 5" row="10 / span 1" >
-                <ItemIcon>
-                    <FaBirthdayCake />
-                </ItemIcon>
-                Информатика и вычислительная техника, 2019-2022 гг.
-            </AboutItem>
+            <Wallpaper onClick={numItem => setNumItem(0)}/>
+            <AboutHeading>Меня зовут Марат Сабитов. Я начинающий Frontend-разработчик.</AboutHeading>
+            {itemList}
+            <DepImgCont url={imgArray[numItem]} />
+            <Resume url={ResImg} />
         </AboutContainer>
     )
 }
